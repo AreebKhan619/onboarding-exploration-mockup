35c954c9e04722180512137040d0c77fa018d17e
Areeb Khan <areebkhan619@gmail.com>
2022-11-05 09:59:46 +0530

    Added testing libraries, removed interface duplication

--- src/components/buttons/styled.tsx
+++ src/components/buttons/styled.tsx
@@ -1,27 +1,22 @@
 import styled from "styled-components";
-
-// TODO: Consolidate with index types
-interface CustomButtonProps {
-  variant: "primary" | "secondary" | "tertiary" | "ghost";
-  wSpan?: "block";
-}
+import { CustomButtonProps } from "./types";
 
 export const CustomButton = styled.button<CustomButtonProps>`
-  width: ${props=>{
+  width: ${(props) => {
     switch (props.wSpan) {
       case "block":
-        return "100%"
+        return "100%";
       default:
         break;
     }
   }};
   cursor: pointer;
-  padding: ${props=>props.theme.paddingMargins.medium};
-  background-color: ${props=>props.theme.colors.purplePrimary};
-  color: ${props=>props.theme.colors.white};
+  padding: ${(props) => props.theme.paddingMargins.medium};
+  background-color: ${(props) => props.theme.colors.purplePrimary};
+  color: ${(props) => props.theme.colors.white};
   border: none;
-  border-radius: ${props=>props.theme.borderRadius.small};
-  &:hover{
+  border-radius: ${(props) => props.theme.borderRadius.small};
+  &:hover {
     filter: brightness(0.9);
   }
 `;
